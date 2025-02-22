c25b37bce7d5ab9fbea5b18eafed21256e6608b6
Francisco Rodriguez <franrodriguez943@gmail.com>
2025-02-22 17:36:08 -0300

    graph fix

--- src/componentes/MiComponente.jsx
+++ src/componentes/MiComponente.jsx
@@ -3,7 +3,7 @@ import Grafica from './Grafica'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate, NavLink, Navigate } from "react-router-dom";
 import Menu from "./Menu";
-import { cargarRegistros , cargarActividades } from '../redux/registrosSlice'
+import { cargarRegistros, cargarActividades } from '../redux/registrosSlice'
 import { Container, Row, Col, Card, Button, Form } from "react-bootstrap";
 import { useAuth } from "../hooks/useAuth";
 
@@ -13,24 +13,45 @@ const MiComponente = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const { comprobarLogin } = useAuth();
-    
+
 
     const registros = useSelector((state) => state.registros.lista);
-    const actividades = useSelector((state) => state.registros.actividades) || [];
+    const actividades = useSelector((state) => state.registros.actividades);
 
-    const tiempos = registros.map(registro => registro.tiempo);
-    const nombreActividades = actividades.map(actividad => actividad.nombre);
+    
+    const registrosFiltrados = registros.filter(registro => registro.tiempo > 0);
 
-    console.log('tiempos', tiempos)
-    console.log('nombreActividades', nombreActividades)
+   
+    const actividadesTotales = registrosFiltrados.reduce((acc, registro) => {
+        const { idActividad, tiempo } = registro;
+
+        if (!acc[idActividad]) {
+            acc[idActividad] = { tiempoTotal: 0 };
+        }
+
+        acc[idActividad].tiempoTotal += tiempo;
+        return acc;
+    }, {});
+
+    
+    const nombreActividades = [];
+    const tiempos = [];
+
+    actividades.forEach(actividad => {
+        if (actividadesTotales[actividad.id]) {
+            nombreActividades.push(actividad.nombre);
+            tiempos.push(actividadesTotales[actividad.id].tiempoTotal);
+        }
+    });
+
 
 
     useEffect(() => {
-        
+
         comprobarLogin();
         dispatch(cargarRegistros());
         dispatch(cargarActividades());
-        
+
     }, [dispatch])
 
 
@@ -38,16 +59,16 @@ const MiComponente = () => {
 
         <Container>
 
-        <>
-            <Menu />    
-            <Grafica etiquetas={nombreActividades} datos={tiempos}></Grafica>
+            <>
+                <Menu />
+                <Grafica etiquetas={nombreActividades} datos={tiempos}></Grafica>
 
-        </>
+            </>
 
 
 
         </Container>
-        
+
 
     )
 }
